f2e33ebd26c6af03f79e843c44992a8f1b00473c
jacobballard <jacob.ballard.1999@gmail.com>
2023-06-09 03:40:38 -0400

    Lots of big changes...

--- lib/src/account/create_product/cubit/create_product_cubit.dart
+++ lib/src/account/create_product/cubit/create_product_cubit.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
 import 'package:pastry/src/account/create_product/cubit/product_details_cubit.dart';
+import 'package:repositories/repositories.dart';
 
 import '../../utilities/delivery_methods/delivery_method_cubit.dart';
 import '../../utilities/image_upload/image_uploader_cubit.dart';
@@ -15,6 +18,9 @@ class CreateProductCubit extends Cubit<CreateProductState> {
   final ModifierCubit modifierCubit;
   final DeliveryMethodsCubit deliveryMethodsCubit;
   final ImageUploaderCubit imageUploaderCubit;
+  final String productId;
+  final CreateProductRepository createProductRepository;
+  final AuthenticationRepository authenticationRepository;
   late final StreamSubscription _productDetailsSubscription;
   late final StreamSubscription _modifierSubscription;
   late final StreamSubscription _deliveryMethodsSubscription;
@@ -24,8 +30,14 @@ class CreateProductCubit extends Cubit<CreateProductState> {
     required this.modifierCubit,
     required this.deliveryMethodsCubit,
     required this.imageUploaderCubit,
-  }) : super(CreateProductState()) {
+    required this.createProductRepository,
+    required this.authenticationRepository,
+    required this.productId,
+  }) : super(const CreateProductState()) {
     _productDetailsSubscription = productDetailsCubit.stream.listen((state) {
+      print("prod det");
+      print(state.title.value);
+      print(this.state.productModifiersStatus);
       emit(CreateProductState(
           productDetailsStatus: state.status,
           productModifiersStatus: this.state.productModifiersStatus,
@@ -34,6 +46,8 @@ class CreateProductCubit extends Cubit<CreateProductState> {
     });
 
     _modifierSubscription = modifierCubit.stream.listen((state) {
+      print("mod sub");
+      print(state.status);
       emit(CreateProductState(
           productDetailsStatus: this.state.productDetailsStatus,
           productModifiersStatus: state.status,
@@ -66,8 +80,38 @@ class CreateProductCubit extends Cubit<CreateProductState> {
     return super.close();
   }
 
-  void submit() {
+  Future<void> submit() async {
     if (!state.isValidated) return;
-    // TODO : Call repo to submit here
+
+    print("submitting");
+    emit(state.copyWith(status: FormzStatus.submissionInProgress));
+    try {
+      await createProductRepository.create(
+        token: await authenticationRepository.getIdToken(),
+        title: productDetailsCubit.state.title.value,
+        description: productDetailsCubit.state.description.value,
+        price: productDetailsCubit.state.price.value,
+        deliveryMethods: deliveryMethodsCubit.state.methods,
+        modifiers: modifierCubit
+            .state.modifiers, // Assuming you have a modifiers field in state
+        // Add other product details here as needed
+      );
+
+      emit(state.copyWith(status: FormzStatus.submissionSuccess));
+    } on CreateProductFailure catch (e) {
+      // Assuming you have a CreateProductFailure exception class similar to CreateStoreFailure
+      emit(
+        state.copyWith(
+          errorMessage: e.message,
+          status: FormzStatus.submissionFailure,
+        ),
+      );
+    } catch (_) {
+      emit(
+        state.copyWith(
+          status: FormzStatus.submissionFailure,
+        ),
+      );
+    }
   }
 }
